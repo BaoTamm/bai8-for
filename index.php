2956edf9d74881b26309d640fd9e2a75b7b6af83
BaoTamm <97217981+BaoTamm@users.noreply.github.com>
2022-02-24 16:11:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+   //Tinh tong tu 1 den 100
+   $tong = 0;
+   for($i = 0; $i <= 100; $i++){
+       $tong += $i;
+   }
+   echo "i: $i <br>";
+   echo "Tong: $tong";
+   */
+   //Tinh tong tu 1 den 100
+ /*  
+   $tong = 0;
+   for($i = 0; $i <= 100; $i++){
+       if($i > 50) break;
+       $tong += $i;
+   }
+   echo "i: $i <br>";
+   echo "Tong: $tong"
+   */
+   $colors = array('red', 'green', 'blue', 'yellow');
+   foreach($colors as $value){
+      echo "$value <br>";
+  }
+
+  for($i=0; $i < count($colors); $i++){
+      echo "$colors[$i] <br>";
+  }
+?>
